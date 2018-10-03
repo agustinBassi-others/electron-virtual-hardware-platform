73be22691d2348f00e2f8cbcc4102f0d0abc4bdf
Agustin <jagustinbassi@gmail.com>
2018-10-03 00:23:25 -0300

    Agrega encabezado con licencia al firmware

--- firm/src/virtual_hardware.c
+++ firm/src/virtual_hardware.c
@@ -1,8 +1,44 @@
+/* Copyright 2018, Agustin Bassi.
+ * All rights reserved.
+ *
+ * This file is part ViHard library, a library of virtual hardware
+ * for Embedded Systems.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* Date: 2018-10-02 */
+
 /*==================[inclusions]=============================================*/
 
 #include <math.h>
 
-#include "virtual_hardware.h"
+#include "vihard.h"
 
 /*==================[macros and definitions]=================================*/
 
@@ -136,7 +172,7 @@ static void UartWriteString (char * string)
  * Funcion propia para llamar al delay de la plataforma.
  * @param delayMs
  */
-static void DelayUs (volatile uint32_t microSecs)
+static void DelayUs (uint32_t microSecs)
 {
     volatile uint64_t i;
     volatile uint64_t calculatedDelay;
